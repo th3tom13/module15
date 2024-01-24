f0ad3347e29a43a48cfe5dab2af5aede5695af14
th3tom13 <156960974+th3tom13@users.noreply.github.com>
2024-01-24 15:14:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var map = L.map('map').setView([37.75, -122.48], 13);
+
+var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+$.getJSON('https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson', function(data) {
+  var coordinatesOnly = data.features.map(function(feature) {
+    return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
+  });
+
+  var heat = L.heatLayer(coordinatesOnly).addTo(map);
+});
